284c3f5ec560c7754e125c14c9090a7cceefe6ce
Karl Nordstrom <1003412n@student.gla.ac.uk>
2015-03-29 02:46:55 +0100

    Cleaned up a bit.

--- src/metropolis.c
+++ src/metropolis.c
@@ -2,8 +2,8 @@
 
 Grid allocateGrid(const size_t gridsize) {
   MatrixSU3 *matrix = (MatrixSU3 *) malloc ( gridsize * gridsize * gridsize * gridsize * sizeof(MatrixSU3 *) );
-  size_t i = 0;
-  while(i < gridsize * gridsize * gridsize * gridsize) { *(matrix + i) = allocateSU3(); ++i; }
+  short i = -1;
+  while(++i < gridsize * gridsize * gridsize * gridsize) *(matrix + i) = allocateSU3();
   Grid grid = { matrix, gridsize };
   return grid;
 }
@@ -11,3 +11,4 @@ Grid allocateGrid(const size_t gridsize) {
 MatrixSU3 findGridPoint(const Grid grid, const size_t x, const size_t y, const size_t z, const size_t t) {
   return *(grid.grid + (x % grid.gridsize) +  grid.gridsize * (y % grid.gridsize) + grid.gridsize * grid.gridsize * (z % grid.gridsize) + grid.gridsize * grid.gridsize * grid.gridsize * (t % grid.gridsize));
 }
+
